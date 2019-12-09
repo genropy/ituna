44b8d4f3fe825ac9712e44add5b3a39d6153d813
Saverio Porcari <>
2019-12-09 15:48:22 +0100

    no message

--- packages/ituna/model/genres.py
+++ packages/ituna/model/genres.py
@@ -5,3 +5,4 @@ class Table(object):
         tbl =  pkg.table('genres',legacy_name='main.genres',legacy_db='chinook',pkey='genreid',caption_field='name',name_long='genres',name_plural='genres')
         tbl.column('genreid',dtype='I',name_long='Id',legacy_name='GenreId')
         tbl.column('name',size='0:120',name_long='Name',legacy_name='Name')
+        tbl.formulaColumn('tot_genre', select=dict(columns='SUM($totprice)', table='ituna.invoice_items', where='@trackid.genreid = #THIS.genreid') , name_long='Tot genre')
